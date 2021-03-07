3176f852ad618812a2d673bd213f863f5487900b
skapoor3@ucsc.edu <skapoor3@ucsc.edu>
2021-03-08 05:03:05 +0530

    Reflex agent that works but needs to be improved

--- pacman-master/pacai/student/myTeam.py
+++ pacman-master/pacai/student/myTeam.py
@@ -3,11 +3,14 @@ from pacai.agents.capture.capture import CaptureAgent
 from pacai.util import counter
 from pacai.util import probability
 from pacai.util import util
+from pacai.util import stack, queue, priorityQueue
+# from pacai.student.qlearningAgents import ApproximateQAgent
+from pacai.core.actions import Actions
 import random
 
 def createTeam(firstIndex, secondIndex, isRed,
-        first = 'pacai.agents.capture.defense.DefensiveReflexAgent',
-        second = 'pacai.agents.capture.offense.OffensiveReflexAgent'):
+        first = 'pacai.agents.capture.dummy.DummyAgent',
+        second = 'pacai.agents.capture.dummy.DummyAgent'):
     """
     This function should return a list of two agents that will form the capture team,
     initialized using firstIndex and secondIndex as their agent indexed.
@@ -15,140 +18,73 @@ def createTeam(firstIndex, secondIndex, isRed,
     and will be False if the blue team is being created.
     """
 
-    firstAgent = reflection.qualifiedImport(first)
-    secondAgent = reflection.qualifiedImport(second)
+    # firstAgent = reflection.qualifiedImport(first)
+    # secondAgent = reflection.qualifiedImport(second)
+    firstAgent = MyAgent
+    secondAgent = MyAgent
 
     return [
         firstAgent(firstIndex),
         secondAgent(secondIndex),
     ]
 
-class QLAgent(CaptureAgent):
-    """
-    Base Q learning agent that will be used for
-    both of the agents.
-    """
+class MyAgent(CaptureAgent):
+
+    # def aStarSearch(problem, heuristic):
+    #     """
+    #     Search the node that has the lowest combined cost and heuristic first.
+    #     """
+
+    #     # *** Your Code Here ***
+    #     state_pq = priorityQueue.PriorityQueue()
+    #     visited = []
+    #     start_state = problem.startingState()
+    #     # initial state pushed to priority queue
+    #     state_pq.push((start_state, [], 0), heuristic(start_state, problem))
+
+    #     while not state_pq.isEmpty():
+    #         curr_state, action_list, total_cost = state_pq.pop()
+    #         if problem.isGoal(curr_state):  # if goal is reached return list of actions to take
+    #             return action_list
+
+    #         if curr_state not in visited:
+    #             for succ in problem.successorStates(curr_state):
+    #                 state, action, cost = succ
+
+    # # push all non-visited successor states to queue and update action list,
+    # # action cost and get estimated action cost using heuristic function
+    #                 if state not in visited:
+    #                     state_pq.push((state, action_list + [action], total_cost + cost),
+    #                     problem.actionsCost(action_list + [action]) + heuristic(state, problem))
+    #         visited.append(curr_state)
+
+    #     return action_list
+
     def __init__(self, index, **kwargs):
         super().__init__(index)
-        self.values = counter.Counter()
-        self.features = None
-        self.weights = None
-        self.alpha = None
-        self.epsilon = None
-        self.discount = None
 
-    def registerInitialState(self, state):
+    def chooseAction(self, gameState):
         """
-        This function is called to set up our Agents
-        So make sure our two agents override this and call
-        super().regiserInitialState(state)
-        """
-        super().registerInitialState(state)
-    
-    def getValue(self, state):
-        """
-        Return the value of the highest valued action
-        "action" should be received through getPolicy
-        """
-        action = self.getPolicy(state)
-        value = 0.0
-        if action is not None:
-            value = self.getQValue(state, action)
-        return value
-
-    def getQValue(self, state, action):
-        """
-        Q value is the dot product of weights and features 
-        """
-        weights = self.weights
-        features = self.getFeatures(state, action)
-        return weights * features
-
-    def getFeatures(self, state, action):
-        """
-        Each of the agent class should override this function
-        and define their own features. This will be used in
-        getQValue and update function. Returns a "Counter" dictionary
+        Picks among the actions with the highest return from `ReflexCaptureAgent.evaluate`.
         """
 
-        """
-        note: maybe look at this:
-        pacai.core.featureExtractors.IdentityExtractor
-        To model our getFeatures function
-        Though we will have to come up with features that would work
-        with our attack/defense agent separately since they're going to
-        behave differently
-        """
-        return self.features
+        actions = gameState.getLegalActions(self.index)
 
-    def getPolicy(self, state):
-        """
-        This function should return the best action
-        available to our agent, this shoud be evaluated
-        using the self.getQValue function
-        """
-        action = None
-        legalActions = state.getLegalActions(state)[self.index]
-        if legalActions:
-            maxValue = float("-inf")
-            for legalAction in legalActions:
-                #  note: might wanna skip Directions.STOP here
-                #  though for now just leaving it in
-                qValue = self.getQValue(state, legalAction)
-                nextState = self.getSuccessor(state, legalAction)
-                #  reward should be calculated here based on the score
-                #  of current state and next state potentially right now just 0
-                reward = 0
-                #  calling update here:
-                #  this should probably be commented out for when
-                #  we submit our agent
-                self.update(state, action, nextState, reward)
-                if (maxValue < qValue):
-                    maxValue = qValue
-                    action = legalAction
-        return action
+        # start = time.time()
+        values = [self.evaluate(gameState, a) for a in actions]
+        # logging.debug('evaluate() time for agent %d: %.4f' % (self.index, time.time() - start))
 
-    def chooseAction(self, state):
-        """
-        This is the function we're supposed to override to
-        return a legal action.
-        """
-        legalActions = state.getLegalActions(self.index)
-        if not legalActions:
-            return None
-        else:
-            if probability.flipCoin(self.epsilon):
-                action = random.choice(legalActions)
-            else:
-                action = self.getPolicy(state)
-        return action
+        maxValue = max(values)
+        bestActions = [a for a, v in zip(actions, values) if v == maxValue]
 
-    def update(self, state, action, nextState, reward):
-        """
-        This function updates all the weights for our features
-        It should be called in getPolicy.
-        """
+        return random.choice(bestActions)
 
-        """
-        Formula for the correction value:
-        w <- w + α[correction]f(s, a)
-        correction = (R(s,a) + 𝛾V(s')) - Q(s, a)
-        """
-        features = self.getFeatures(state, action)
-        for feature in features:
-            discount = self.discount
-            qPrime = self.getValue(nextState)
-            qValue = self.getQValue(state, action)
-            alpha = self.alpha
-            correction = reward + discount  * qPrime - qValue
-            self.weights[feature] += alpha * correction * features[feature]
-
-    def getSuccessor(self, state, action):
+    def getSuccessor(self, gameState, action):
         """
         Finds the next successor which is a grid position (location tuple).
-        ** Coppied from the Reflex Agent class **
         """
-        successor = state.generateSuccessor(self.index, action)
+
+        successor = gameState.generateSuccessor(self.index, action)
         pos = successor.getAgentState(self.index).getPosition()
 
         if (pos != util.nearestPoint(pos)):
@@ -156,24 +92,62 @@ class QLAgent(CaptureAgent):
             return successor.generateSuccessor(self.index, action)
         else:
             return successor
-    """
-    Not sure what to do with this function yet
-    Though it is called at the end of the game
-    So our agents should override this
-    and probably print it to a file
-    def final(self, state):
-    """
 
-class AttackAgent(QLAgent):
-    """
-    Extends QLAgent to implement Attack Agent
-    # TODO: override registerInitialState(self, state):
-            override getFeatures(self, state, action)
-    """
+    def evaluate(self, gameState, action):
+        """
+        Computes a linear combination of features and feature weights.
+        """
 
-class DefenseAgent(QLAgent):
-    """
-    Extends QLAgent to implement Defense Agent
-    # TODO: override registerInitialState(self, state)
-            override getFeatures(self, state, action)
-    """
+        features = self.getFeatures(gameState, action)
+        weights = self.getWeights(gameState, action)
+
+        return features * weights
+
+    def getFeatures(self, gameState, action):
+        """
+        Returns a dict of features for the state.
+        The keys match up with the return from `ReflexCaptureAgent.getWeights`.
+        """
+        food = self.getFood(gameState)
+        walls = gameState.getWalls()
+        ghosts = self.getOpponents(gameState)
+        ghost_pos = [gameState.getAgentState(g).getPosition() for g in ghosts]
+
+        features = counter.Counter()
+        successor = self.getSuccessor(gameState, action)
+        features['successorScore'] = self.getScore(successor)
+
+        next_x, next_y = successor.getAgentState(self.index).getPosition()
+        next_x, next_y = int(next_x), int(next_y)
+
+        # Count the number of ghosts 1-step away.
+        features["#-of-ghosts-1-step-away"] = sum((next_x, next_y) in
+                Actions.getLegalNeighbors(g, walls) for g in ghost_pos)
+
+        # If there is no danger of ghosts then add the food feature.
+        if not features["#-of-ghosts-1-step-away"] and food[next_x][next_y]:
+            features["eats-food"] = 1.0
+
+        # Compute distance to the nearest food.
+        foodList = self.getFood(successor).asList()
+
+        # This should always be True, but better safe than sorry.
+        if (len(foodList) > 0):
+            myPos = successor.getAgentState(self.index).getPosition()
+            minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
+            features['distanceToFood'] = 1 / minDistance
+
+        return features
+
+    def getWeights(self, gameState, action):
+        """
+        Returns a dict of weights for the state.
+        The keys match up with the return from `ReflexCaptureAgent.getFeatures`.
+        """
+
+        return {
+            'successorScore': 10.0,
+            'distanceToFood': 5.0,
+            '#-of-ghosts-1-step-away': -100,
+            'eats-food': 0.0
+        }
